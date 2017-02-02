3bbf5b424d7b4a84b7e201b1901b5ce6d8dade27
Sargon Piraev <sargonpiraev@gmail.com>
2017-02-02 12:29:31 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import cors from 'cors';
 
 import db from './model';
 import departmentRouter from './controller/department';
 
 const app = express();
 
-// app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
